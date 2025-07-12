6e5894c78c09ff6e974d485ec41a71917df65344
Ajeet <Ajeet@DESKTOP-FDDB765>
2025-07-12 20:31:36 +0530

    first commit

---
+++ JenkinChecking/src/main/java/JenkinChecking/JenkinChecking/App.java
@@ -0,0 +1,10 @@
+package JenkinChecking.JenkinChecking;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
